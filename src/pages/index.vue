c689662e789d6d795e9d38699bc3f531616067bf
ZulfaBueraheng <zulfa.bg@gmail.com>
2024-05-20 01:05:25 +0700

    update index & popular

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@
       <NavBar />
       <v-container>
         <v-row>
-          <v-col cols="12">
+          <v-col cols="12" sm="12">
             <v-carousel>
               <v-carousel-item
                 v-for="(item, i) in items"
@@ -14,13 +14,26 @@
               >
                 <v-row class="fill-height">
                   <v-col cols="12" sm="6">
-                    <div class="d-flex fill-height justify-center align-center">
+                    <div
+                      class="d-flex flex-column justify-center align-center text-center"
+                    >
                       <h2 class="text-white top">{{ item.title }}</h2>
+                      <p class="text-white text">{{ item.text }}</p>
+                      <v-btn class="btn-buy">Buy Now</v-btn>
                     </div>
                   </v-col>
                 </v-row>
               </v-carousel-item>
             </v-carousel>
+            <v-toolbar color="transparent">
+              <v-toolbar-title>Popular Product</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon="mdi mdi-chevron-left"></v-btn>
+              <v-btn icon="mdi mdi-chevron-right"></v-btn>
+            </v-toolbar>
+            <v-col cols="12" sm="12" class="mt-n10">
+              <Popular />
+            </v-col>
           </v-col>
         </v-row>
       </v-container>
@@ -30,6 +43,7 @@
 
 <script setup>
 import NavBar from "../components/NavBar.vue";
+import Popular from "../components/Popular.vue";
 </script>
 
 <script>
@@ -38,37 +52,37 @@ export default {
     return {
       items: [
         {
-          src: "1.jpg",
+          src: "image/1.jpg",
           title: "Zulfa Studio",
           text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         },
         {
-          src: "2.jpg",
+          src: "image/2.jpg",
           title: "Zulfa Studio",
           text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         },
         {
-          src: "3.jpg",
+          src: "image/3.jpg",
           title: "Zulfa Studio",
           text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         },
         {
-          src: "4.jpg",
+          src: "image/4.jpg",
           title: "Zulfa Studio",
           text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         },
         {
-          src: "5.jpg",
+          src: "image/5.jpg",
           title: "Zulfa Studio",
           text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         },
         {
-          src: "6.jpg",
+          src: "image/6.jpg",
           title: "Zulfa Studio",
           text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         },
         {
-          src: "7.jpg",
+          src: "image/7.jpg",
           title: "Zulfa Studio",
           text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         },
@@ -83,8 +97,11 @@ export default {
   margin-top: 100px;
 }
 .text {
-  margin-top: -40px;
+  margin-top: 20px;
   margin-left: 80px;
+}
+.btn-buy {
+  margin-top: 20px;
 }
 .top-day {
   margin-top: 120px;
