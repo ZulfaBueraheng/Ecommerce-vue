408968dceaecea79585732e4609e69cc51a0babc
ZulfaBueraheng <zulfa.bg@gmail.com>
2024-05-20 13:16:22 +0700

    Update Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-footer>
+      <v-card flat class="white--text py-12 px-5" width="100%" color="#F4F5F9">
+        <v-row>
+          <v-col cols="12" xs="12" sm="6" md="4" class="pa-16">
+            <div class="text-center">
+              <span>S</span><strong>hopio</strong>
+            </div>
+            <v-card-text class="mt-2">
+              <div class="text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </div>
+            </v-card-text>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="2">
+            <v-card-text class="white--text pt-0">
+              <h3>About</h3>
+            </v-card-text>
+            <v-card-text class="grey--text"> How it Work</v-card-text>
+            <v-card-text class="grey--text mt-n4"> Our Promrams </v-card-text>
+            <v-card-text class="grey--text mt-n4"> Promotions </v-card-text>
+            <v-card-text class="grey--text mt-n4"> Refer A Friend </v-card-text>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="2">
+            <v-card-text class="white--text pt-0">
+              <h3>Dellvery</h3>
+            </v-card-text>
+            <v-card-text class="grey--text"> Cost on Delevery </v-card-text>
+            <v-card-text class="grey--text mt-n4"> Payment Method </v-card-text>
+            <v-card-text class="grey--text mt-n4"> Delevery Astus </v-card-text>
+            <v-card-text class="grey--text mt-n4"> Returns </v-card-text>
+          </v-col>
+          <v-col cols="12" xs="12" sm="6" md="4">
+            <v-card-text class="white--text pt-0">
+              <h3>Contact</h3>
+            </v-card-text>
+            <v-card-text class="grey--text"> Zulfa </v-card-text>
+            <v-card-text class="grey--text mt-n4">
+              Narathiwat
+            </v-card-text>
+            <v-card-text class="grey--text mt-n4"> zulfazzz.com </v-card-text>
+            <v-card-text class="grey--text mt-n4"> +66805419507 </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-footer>
+  </template>
+  
+  <script>
+  export default {
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -47,8 +47,88 @@
           <v-col cols="12" sm="12" class="mt-n10">
             <Client />
           </v-col>
+          <v-col cols="12" sm="12" class="mt-n10">
+            <v-row>
+              <v-col cols="12" sm="4">
+                <v-card class="mx-auto my-12 pb-4" max-width="374">
+                  <v-row>
+                    <v-col cols="12" sm="6">
+                      <v-card-item class="mt-10">
+                        <v-card-title class="text-center">
+                          Smart watch
+                        </v-card-title>
+                      </v-card-item>
+                        <v-card-text>
+                          <div class="text-center">
+                            Small paltes, salades & sandwitches
+                          </div>
+                          <div class="text-center">
+                            <v-btn color="black">But Now</v-btn>
+                          </div>
+                        </v-card-text>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-img height="250" class="mx-4" src="/image/2.jpg">
+                      </v-img>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-card class="mx-auto my-12 pb-4" max-width="374">
+                  <v-row>
+                    <v-col cols="12" sm="6">
+                      <v-card-item class="mt-10">
+                        <v-card-title class="text-center">
+                          Smart watch
+                        </v-card-title>
+                      </v-card-item>
+                        <v-card-text>
+                          <div class="text-center">
+                            Small paltes, salades & sandwitches
+                          </div>
+                          <div class="text-center">
+                            <v-btn color="black">But Now</v-btn>
+                          </div>
+                        </v-card-text>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-img height="250" class="mx-4" src="/image/2.jpg">
+                      </v-img>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-card class="mx-auto my-12 pb-4" max-width="374">
+                  <v-row>
+                    <v-col cols="12" sm="6">
+                      <v-card-item class="mt-10">
+                        <v-card-title class="text-center">
+                          Smart watch
+                        </v-card-title>
+                      </v-card-item>
+                        <v-card-text>
+                          <div class="text-center">
+                            Small paltes, salades & sandwitches
+                          </div>
+                          <div class="text-center">
+                            <v-btn color="black">But Now</v-btn>
+                          </div>
+                        </v-card-text>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-img height="250" class="mx-4" src="/image/2.jpg">
+                      </v-img>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
         </v-row>
       </v-container>
+      <Footer />
     </v-main>
   </v-app>
 </template>
@@ -58,7 +138,7 @@ import NavBar from "../components/NavBar.vue";
 import Popular from "../components/Popular.vue";
 import Featured from "../components/Featured.vue";
 import Client from '../components/Client.vue';
-
+import Footer from "@/components/Footer.vue";
 </script>
 
 <script>
